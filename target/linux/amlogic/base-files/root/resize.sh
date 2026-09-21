61468e9da0dae39429ecbf0cfaeea8b84db5dab9
yangjianjun1 <yangjianjun8656@petalmail.com>
2026-09-21 14:41:14 +0800

    Update resize.sh

--- target/linux/amlogic/base-files/root/resize.sh
+++ target/linux/amlogic/base-files/root/resize.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
-resize2fs /dev/mmcblk1p2
+parted /dev/mmcblk1 resizepart 4 100%
+resize2fs /dev/mmcblk1p4
 echo "# Put your custom commands here that should be executed once
 # the system init finished. By default this file does nothing.
-
 exit 0">/etc/rc.local
 rm -rf /root/resize.sh
