076bfd8107937b4451c76aa74d0bead5cacd3b71
yangjianjun1 <yangjianjun8656@petalmail.com>
2026-09-21 19:29:01 +0800

    Delete target/linux/amlogic/base-files/root/resize.sh

--- target/linux/amlogic/base-files/root/resize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-parted /dev/mmcblk1 resizepart 4 100%
-resize2fs /dev/mmcblk1p4
-echo "# Put your custom commands here that should be executed once
-# the system init finished. By default this file does nothing.
-exit 0">/etc/rc.local
-rm -rf /root/resize.sh
