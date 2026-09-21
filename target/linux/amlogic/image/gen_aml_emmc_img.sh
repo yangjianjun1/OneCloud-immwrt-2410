10a7dc7a4441ef9e2e3bd47ba1f1cbdefe0e3a8a
yangjianjun1 <yangjianjun8656@petalmail.com>
2026-09-21 14:33:51 +0800

    Update gen_aml_emmc_img.sh

--- target/linux/amlogic/image/gen_aml_emmc_img.sh
+++ target/linux/amlogic/image/gen_aml_emmc_img.sh
@@ -2,28 +2,54 @@
 # SPDX-License-Identifier: GPL-2.0-only
 #
 # Copyright (C) 2017 OpenWrt.org
-
 set -x
-[ $# -eq 5 ] || {
-    echo "SYNTAX: $0 <file> <bootfs image> <rootfs image> <bootfs size> <rootfs size>"
+[ $# -eq 6 ] || {
+    echo "SYNTAX: $0 <file> <bootfs image> <rootA image> <bootfs size> <rootA size> <rootB size>"
     exit 1
 }
-
 OUTPUT="$1"
 BOOTFS="$2"
-ROOTFS="$3"
+ROOTFS_A="$3"
 BOOTFSSIZE="$4"
-ROOTFSSIZE="$5"
+ROOTFSSIZE_A="$5"
+ROOTFSSIZE_B="$6"
+UPGRADE_SIZE="512"
 
 head=4
 sect=2048
+# -t 6 = FAT16 type 0x06
+set $(ptgen -o $OUTPUT -h $head -s $sect -l 4096 \
+-t 6 -p ${BOOTFSSIZE}M \
+-t 83 -p ${ROOTFSSIZE_A}M \
+-t 83 -p ${ROOTFSSIZE_B}M \
+-t 83 -p ${UPGRADE_SIZE}M )
 
-set $(ptgen -o $OUTPUT -h $head -s $sect -l 4096 -t c -p ${BOOTFSSIZE}M -t 83 -p ${ROOTFSSIZE}M)
-
+# ptgen输出：p1off p1sz p2off p2sz p3off p3sz p4off p4sz
 BOOTOFFSET="$(($1 / 512))"
 BOOTSIZE="$(($2 / 512))"
-ROOTFSOFFSET="$(($3 / 512))"
-ROOTFSSIZE="$(($4 / 512))"
+ROOTA_OFFSET="$(($3 / 512))"
+ROOTA_SIZE="$(($4 / 512))"
+ROOTB_OFFSET="$(($5 / 512))"
+ROOTB_SIZE="$(($6 / 512))"
+UPG_OFFSET="$(($7 / 512))"
+UPG_SIZE="$(($8 / 512))"
 
 dd bs=512 if="$BOOTFS" of="$OUTPUT" seek="$BOOTOFFSET" conv=notrunc
-dd bs=512 if="$ROOTFS" of="$OUTPUT" seek="$ROOTFSOFFSET" conv=notrunc
+dd bs=512 if="$ROOTFS_A" of="$OUTPUT" seek="$ROOTA_OFFSET" conv=notrunc
+
+# 创建空白rootB
+TMP_ROOTB=$(mktemp)
+dd if=/dev/zero of="$TMP_ROOTB" bs=512 count="${ROOTB_SIZE}"
+mkfs.ext4 "$TMP_ROOTB"
+dd bs=512 if="$TMP_ROOTB" of="$OUTPUT" seek="$ROOTB_OFFSET" conv=notrunc
+rm -f "$TMP_ROOTB"
+
+# 创建空白upgrade分区
+TMP_UPG=$(mktemp)
+dd if=/dev/zero of="$TMP_UPG" bs=512 count="${UPG_SIZE}"
+mkfs.ext4 "$TMP_UPG"
+dd bs=512 if="$TMP_UPG" of="$OUTPUT" seek="$UPG_OFFSET" conv=notrunc
+rm -f "$TMP_UPG"
+
+sync
+echo "Done: p1(FAT16 ${BOOTFSSIZE}M), p2(rootA ${ROOTFSSIZE_A}M), p3(rootB ${ROOTFSSIZE_B}M), p4(upgrade ${UPGRADE_SIZE}M)"
