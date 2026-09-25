607ac7dc4c5b010b328570f4a5fb7959fe94256e
yangjianjun1 <yangjianjun8656@petalmail.com>
2026-09-25 21:04:46 +0800

    Update op2.sh

--- op2.sh
+++ op2.sh
@@ -9,14 +9,11 @@
 # File name: diy-op2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-set -eo pipefail
 
 # 删除自带的 golang
 rm -rf feeds/packages/lang/golang
 # 拉取新的 golang
-git clone --depth 1 https://github.com/sbwml/packages_lang_golang.git -b 26.x feeds/packages/lang/golang
-
-mkdir -p package/chajian
+git clone https://github.com/sbwml/packages_lang_golang.git -b 26.x feeds/packages/lang/golang
 # 拉取 luci-app-poweroffdevice（master 分支即 24.10 JS 版）
 git clone --depth 1 https://github.com/sirpdboy/luci-app-poweroffdevice.git package/chajian/poweroffdevice
 # 拉取 luci-app-mosdns（含 mosdns 主程序 + v2dat）
@@ -42,19 +39,41 @@ cd ../../..
 #====修正！！是 bin 不是 sbin====
 CFG_GEN="package/base-files/files/bin/config_generate"
 
+## 筛选程序
+function merge_package(){
+    # 参数1是分支名,参数2是库地址。所有文件下载到指定路径。
+    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+    trap 'rm -rf "$tmpdir"' EXIT
+    branch="$1" curl="$2" target_dir="$3" && shift 3
+    rootdir="$PWD"
+    localdir="$target_dir"
+    [ -d "$localdir" ] || mkdir -p "$localdir"
+    tmpdir="$(mktemp -d)" || exit 1
+    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+    cd "$tmpdir"
+    git sparse-checkout init --cone
+    git sparse-checkout set "$@"
+    for folder in "$@"; do
+        mv -f "$folder" "$rootdir/$localdir"
+    done
+    cd "$rootdir"
+}
+
 # 修改默认 IP
-sed -i 's/192.168.50.1/192.168.50.23/g' "${CFG_GEN}"
-#sed -i 's/192.168.1.1/192.168.8.1/g' "${CFG_GEN}"
+sed -i 's/192.168.50.1/192.168.50.23/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci-light/Makefile
 
 # 修改主机名
-sed -i "s/hostname='.*'/hostname='OneCloud'/g" "${CFG_GEN}"
-# 修改默认时区
-sed -i "s/timezone='.*'/timezone='CST-8'/g" "${CFG_GEN}"
-sed -i "/.*timezone='CST-8'.*/a\ set system.@system[-1].zonename='Asia/Shanghai'" "${CFG_GEN}"
+sed -i "s/hostname='.*'/hostname='OneCloud'/g" package/base-files/files/bin/config_generate
 
-echo "==== patch check fullcone keyword ====="
-grep fullcone feeds/luci/applications/luci-app-firewall/htdocs/luci-static/resources/view/firewall/zones.js
-echo "==== diy‑op2.sh finish ====="
+# 修改默认时区
+sed -i "s/timezone='.*'/timezone='CST-8'/g" package/base-files/files/bin/config_generate
+sed -i "/.*timezone='CST-8'.*/a\ set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
+
+# 修改固件名称
+sed -i 's/NAME="%D"/NAME="OpenWrt"/g' package/base-files/files/usr/lib/os-release
+sed -i 's/PRETTY_NAME="%D %V"/PRETTY_NAME="OpenWrt %V"/g' package/base-files/files/usr/lib/os-release
+sed -i 's/OPENWRT_RELEASE="%D %V %C"/OPENWRT_RELEASE="OpenWrt %V %C"/g' package/base-files/files/usr/lib/os-release
