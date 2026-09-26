926e91b01490bcc13ffa11e0f0bf708eb6ec7875
yangjianjun1 <yangjianjun8656@petalmail.com>
2026-09-26 09:50:56 +0800

    Update op2.sh

--- op2.sh
+++ op2.sh
@@ -59,3 +59,6 @@ sed -i "/.*timezone='CST-8'.*/a\ set system.@system[-1].zonename='Asia/Shanghai'
 sed -i 's/NAME="%D"/NAME="OpenWrt"/g' package/base-files/files/usr/lib/os-release
 sed -i 's/PRETTY_NAME="%D %V"/PRETTY_NAME="OpenWrt %V"/g' package/base-files/files/usr/lib/os-release
 sed -i 's/OPENWRT_RELEASE="%D %V %C"/OPENWRT_RELEASE="OpenWrt %V %C"/g' package/base-files/files/usr/lib/os-release
+
+# 修复 gen_aml_emmc_img.sh 权限丢失导致 Error 126
+chmod +x target/linux/amlogic/image/gen_aml_emmc_img.sh
